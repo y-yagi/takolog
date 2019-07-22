73e99d29c2cc881e9fb1dab3ab36e3940492413c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-07-22 09:24:02 +0900

    Fetch the latest build when build ID did not specify

--- main.go
+++ main.go
@@ -20,48 +20,107 @@ var (
 	jobCount  = kingpin.Flag("count", "Number of jobs").Default("30").Int()
 )
 
-type responseType struct {
+type JobsResponse struct {
 	Build struct {
-		Jobs struct {
+		Jobs Job
+	}
+}
+
+type PipelineResponse struct {
+	Pipeline struct {
+		Builds struct {
 			Edges []struct {
 				Node struct {
-					Uuid string
+					ID   string
+					Jobs Job
 				}
 			}
 		}
 	}
 }
 
-func main() {
-	kingpin.Parse()
+type Job struct {
+	Edges []struct {
+		Node struct {
+			Uuid string
+		}
+	}
+}
 
-	graphqlClient := graphql.NewClient("https://graphql.buildkite.com/v1")
+const BuildQuery = `
+  query ($slug: ID, $jobCount: Int) {
+    build(slug: $slug) {
+      jobs(first: $jobCount) {
+        edges {
+          node {
+            ... on JobTypeCommand {
+              uuid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-	req := graphql.NewRequest(`
-		query ($slug: ID, $jobCount: Int) {
-      build(slug: $slug) {
-        jobs(first: $jobCount) {
-          edges {
-            node {
-              ... on JobTypeCommand {
-                uuid
+const PipelineQuery = `
+  query ($slug: ID!, $jobCount: Int) {
+		pipeline(slug: $slug) {
+      builds(first: 1) {
+        edges {
+          node {
+						id
+            jobs(first: $jobCount) {
+              edges {
+                node {
+                  ... on JobTypeCommand {
+                    uuid
+                  }
+                }
               }
             }
           }
         }
       }
     }
-	`)
+  }
+`
 
-	req.Var("slug", *buildSlug)
-	req.Var("jobCount", *jobCount)
+func main() {
+	kingpin.Parse()
+
+	var query string
+	var job Job
+	var buildID string
+	if strings.Count(*buildSlug, "/") > 1 {
+		query = BuildQuery
+	} else {
+		query = PipelineQuery
+	}
+
+	graphqlClient := graphql.NewClient("https://graphql.buildkite.com/v1")
+	req := graphql.NewRequest(query)
+
 	auth := fmt.Sprintf("Bearer %s", *apiToken)
 	req.Header.Set("Authorization", auth)
+	req.Var("slug", *buildSlug)
+	req.Var("jobCount", *jobCount)
 	ctx := context.Background()
 
-	var responseData responseType
-	if err := graphqlClient.Run(ctx, req, &responseData); err != nil {
-		log.Fatalf("graphql failed: %s", err)
+	if strings.Count(*buildSlug, "/") > 1 {
+		var jobsResponse JobsResponse
+		if err := graphqlClient.Run(ctx, req, &jobsResponse); err != nil {
+			log.Fatalf("graphql failed: %s", err)
+		}
+		job = jobsResponse.Build.Jobs
+		buildID = strings.Split(*buildSlug, "/")[2]
+	} else {
+		var pipelineResponse PipelineResponse
+		if err := graphqlClient.Run(ctx, req, &pipelineResponse); err != nil {
+			log.Fatalf("graphql failed: %s", err)
+		}
+		job = pipelineResponse.Pipeline.Builds.Edges[0].Node.Jobs
+		buildID = pipelineResponse.Pipeline.Builds.Edges[0].Node.ID
 	}
 
 	config, err := buildkite.NewTokenConfig(*apiToken, *debug)
@@ -74,12 +133,12 @@ func main() {
 	logger := log.New(os.Stdout, "", 0)
 	var wg sync.WaitGroup
 
-	for _, edge := range responseData.Build.Jobs.Edges {
+	for _, edge := range job.Edges {
 		wg.Add(1)
 
 		go func(uuid string) {
 			defer wg.Done()
-			jobLog, _, err := buildkiteClient.Jobs.GetJobLog(jobArgs[0], jobArgs[1], jobArgs[2], uuid)
+			jobLog, _, err := buildkiteClient.Jobs.GetJobLog(jobArgs[0], jobArgs[1], buildID, uuid)
 			if err != nil {
 				errmsg := fmt.Sprintf("Error: %v\n", err)
 				logger.Print(errmsg)
