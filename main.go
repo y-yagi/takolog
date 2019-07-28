63570e3185b185e0dfb850c205185bf69d3f7e77
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-07-28 09:45:40 +0900

    Refactor

--- main.go
+++ main.go
@@ -30,10 +30,7 @@ type PipelineResponse struct {
 	Pipeline struct {
 		Builds struct {
 			Edges []struct {
-				Build struct {
-					ID   string
-					Jobs Jobs
-				} `json:"Node"`
+				Build `json:"Node"`
 			}
 		}
 	}
@@ -45,6 +42,11 @@ type Jobs struct {
 			Uuid string
 		} `json:"Node"`
 	}
+}
+
+type Build struct {
+	ID   string
+	Jobs Jobs
 }
 
 const BuildQuery = `
@@ -89,42 +91,18 @@ const PipelineQuery = `
 func main() {
 	kingpin.Parse()
 
-	var query string
-	var jobs Jobs
-	var buildID string
-	if strings.Count(*buildSlug, "/") > 1 {
-		query = BuildQuery
-	} else {
-		query = PipelineQuery
-	}
-
-	graphqlClient := graphql.NewClient("https://graphql.buildkite.com/v1")
-	if *debug {
-		graphqlClient.Log = func(s string) { log.Println(s) }
-	}
-	req := graphql.NewRequest(query)
-
-	auth := fmt.Sprintf("Bearer %s", *apiToken)
-	req.Header.Set("Authorization", auth)
-	req.Var("slug", *buildSlug)
-	req.Var("jobCount", *jobCount)
-	ctx := context.Background()
+	var build *Build
+	var err error
 
 	if strings.Count(*buildSlug, "/") > 1 {
-		var jobsResponse JobsResponse
-		if err := graphqlClient.Run(ctx, req, &jobsResponse); err != nil {
+		if build, err = fetchBuildByID(BuildQuery); err != nil {
 			log.Fatalf("graphql failed: %s", err)
 		}
-		jobs = jobsResponse.Build.Jobs
-		buildID = strings.Split(*buildSlug, "/")[2]
+		build.ID = strings.Split(*buildSlug, "/")[2]
 	} else {
-		var pipelineResponse PipelineResponse
-		req.Var("state", []string{"PASSED", "FAILED"})
-		if err := graphqlClient.Run(ctx, req, &pipelineResponse); err != nil {
+		if build, err = fetchLatestBuild(PipelineQuery); err != nil {
 			log.Fatalf("graphql failed: %s", err)
 		}
-		jobs = pipelineResponse.Pipeline.Builds.Edges[0].Build.Jobs
-		buildID = pipelineResponse.Pipeline.Builds.Edges[0].Build.ID
 	}
 
 	config, err := buildkite.NewTokenConfig(*apiToken, *debug)
@@ -137,12 +115,12 @@ func main() {
 	logger := log.New(os.Stdout, "", 0)
 	var wg sync.WaitGroup
 
-	for _, edge := range jobs.Edges {
+	for _, edge := range build.Jobs.Edges {
 		wg.Add(1)
 
 		go func(uuid string) {
 			defer wg.Done()
-			jobLog, _, err := buildkiteClient.Jobs.GetJobLog(jobArgs[0], jobArgs[1], buildID, uuid)
+			jobLog, _, err := buildkiteClient.Jobs.GetJobLog(jobArgs[0], jobArgs[1], build.ID, uuid)
 			if err != nil {
 				errmsg := fmt.Sprintf("Error: %v\n", err)
 				logger.Print(errmsg)
@@ -152,4 +130,55 @@ func main() {
 		}(edge.Job.Uuid)
 	}
 	wg.Wait()
+}
+
+func fetchBuildByID(query string) (*Build, error) {
+	var res JobsResponse
+	var build Build
+
+	client := buildClient()
+	req := buildRequest(query)
+	ctx := context.Background()
+
+	if err := client.Run(ctx, req, &res); err != nil {
+		return nil, err
+	}
+
+	build.Jobs = res.Build.Jobs
+	return &build, nil
+}
+
+func fetchLatestBuild(query string) (*Build, error) {
+	var res PipelineResponse
+
+	client := buildClient()
+	req := buildRequest(query)
+	ctx := context.Background()
+
+	req.Var("state", []string{"PASSED", "FAILED"})
+	if err := client.Run(ctx, req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res.Pipeline.Builds.Edges[0].Build, nil
+}
+
+func buildClient() *graphql.Client {
+	graphqlClient := graphql.NewClient("https://graphql.buildkite.com/v1")
+	if *debug {
+		graphqlClient.Log = func(s string) { log.Println(s) }
+	}
+
+	return graphqlClient
+}
+
+func buildRequest(query string) *graphql.Request {
+	req := graphql.NewRequest(query)
+
+	auth := fmt.Sprintf("Bearer %s", *apiToken)
+	req.Header.Set("Authorization", auth)
+	req.Var("slug", *buildSlug)
+	req.Var("jobCount", *jobCount)
+
+	return req
 }
