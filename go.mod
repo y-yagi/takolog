d92a43722efcd351178723a0843b87d7e314f04a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-09-10 09:03:26 +0900

    Use `go-buildkite` only

--- go.mod
+++ go.mod
@@ -1,15 +1,13 @@
 module github.com/y-yagi/takolog
 
 require (
-	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210208195552-ff826a37aa15 // indirect
-	github.com/buildkite/go-buildkite v2.2.1-0.20191023234038-7b3ef074c18c+incompatible
+	github.com/buildkite/go-buildkite/v2 v2.9.0
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/machinebox/graphql v0.2.2
-	github.com/matryer/is v1.4.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
+
+require github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 
 go 1.17
--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"sync"
 
-	"github.com/buildkite/go-buildkite/buildkite"
-	"github.com/machinebox/graphql"
+	"github.com/buildkite/go-buildkite/v2/buildkite"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -20,167 +18,51 @@ var (
 	jobCount  = kingpin.Flag("count", "Number of jobs").Default("120").Int()
 )
 
-type jobsResponse struct {
-	Build struct {
-		Jobs jobs
-	}
-}
-
-type pipelineResponse struct {
-	Pipeline struct {
-		Builds struct {
-			Edges []struct {
-				build `json:"Node"`
-			}
-		}
-	}
-}
-
-type jobs struct {
-	Edges []struct {
-		Job struct {
-			UUID string
-		} `json:"Node"`
-	}
-}
-
-type build struct {
-	ID   string
-	Jobs jobs
-}
-
-const buildQuery = `
-  query ($slug: ID, $jobCount: Int) {
-    build(slug: $slug) {
-      jobs(first: $jobCount) {
-        edges {
-          node {
-            ... on JobTypeCommand {
-              uuid
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-const pipelineQuery = `
-  query ($slug: ID!, $jobCount: Int, $state: [BuildStates!]) {
-		pipeline(slug: $slug) {
-			builds(first: 1, state: $state) {
-        edges {
-          node {
-						id
-            jobs(first: $jobCount) {
-              edges {
-                node {
-                  ... on JobTypeCommand {
-                    uuid
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
 func main() {
 	kingpin.Parse()
 
-	var build *build
+	var build *buildkite.Build
 	var err error
-
-	if strings.Count(*buildSlug, "/") > 1 {
-		if build, err = fetchBuildByID(buildQuery); err != nil {
-			log.Fatalf("graphql failed: %s", err)
-		}
-		build.ID = strings.Split(*buildSlug, "/")[2]
-	} else {
-		if build, err = fetchLatestBuild(pipelineQuery); err != nil {
-			log.Fatalf("graphql failed: %s", err)
-		}
-	}
 
 	config, err := buildkite.NewTokenConfig(*apiToken, *debug)
 	if err != nil {
 		log.Fatalf("client config failed: %s", err)
 	}
-
 	buildkiteClient := buildkite.NewClient(config.Client())
+
+	if build, err = fetchBuildByID(buildkiteClient); err != nil {
+		log.Fatalf("graphql failed: %s", err)
+	}
+
 	jobArgs := strings.Split(*buildSlug, "/")
 	logger := log.New(os.Stdout, "", 0)
 	var wg sync.WaitGroup
 
-	for _, edge := range build.Jobs.Edges {
+	for _, job := range build.Jobs {
 		wg.Add(1)
 
-		go func(uuid string) {
+		go func(id string) {
 			defer wg.Done()
-			if len(uuid) != 0 {
-				jobLog, _, err := buildkiteClient.Jobs.GetJobLog(jobArgs[0], jobArgs[1], build.ID, uuid)
+			if len(id) != 0 {
+				jobLog, _, err := buildkiteClient.Jobs.GetJobLog(jobArgs[0], jobArgs[1], *build.ID, id)
 				if err != nil {
-					errmsg := fmt.Sprintf("Error: %v, ID: %v, uuid: %v\n", err, build.ID, uuid)
+					errmsg := fmt.Sprintf("Error: %v, ID: %v, uuid: %v\n", err, build.ID, id)
 					logger.Print(errmsg)
 				} else {
 					logger.Print(*jobLog.Content)
 				}
 			}
-		}(edge.Job.UUID)
+		}(*job.ID)
 	}
 	wg.Wait()
 }
 
-func fetchBuildByID(query string) (*build, error) {
-	var res jobsResponse
-	var build build
-
-	client := buildClient()
-	req := buildRequest(query)
-	ctx := context.Background()
-
-	if err := client.Run(ctx, req, &res); err != nil {
+func fetchBuildByID(client *buildkite.Client) (*buildkite.Build, error) {
+	slugs := strings.Split(*buildSlug, "/")
+	build, _, err := client.Builds.Get(slugs[0], slugs[1], slugs[2], nil)
+	if err != nil {
 		return nil, err
 	}
 
-	build.Jobs = res.Build.Jobs
-	return &build, nil
-}
-
-func fetchLatestBuild(query string) (*build, error) {
-	var res pipelineResponse
-
-	client := buildClient()
-	req := buildRequest(query)
-	ctx := context.Background()
-
-	req.Var("state", []string{"PASSED", "FAILED"})
-	if err := client.Run(ctx, req, &res); err != nil {
-		return nil, err
-	}
-
-	return &res.Pipeline.Builds.Edges[0].build, nil
-}
-
-func buildClient() *graphql.Client {
-	graphqlClient := graphql.NewClient("https://graphql.buildkite.com/v1")
-	if *debug {
-		graphqlClient.Log = func(s string) { log.Println(s) }
-	}
-
-	return graphqlClient
-}
-
-func buildRequest(query string) *graphql.Request {
-	req := graphql.NewRequest(query)
-
-	auth := fmt.Sprintf("Bearer %s", *apiToken)
-	req.Header.Set("Authorization", auth)
-	req.Var("slug", *buildSlug)
-	req.Var("jobCount", *jobCount)
-
-	return req
+	return build, nil
 }
